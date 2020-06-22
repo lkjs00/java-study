39746f16bdf47369c66be2f02bb829c8c4e33de4
이준현 <junhyun1993@daum.net>
2020-06-22 19:12:21 +0900

    java start

---
+++ Data_and_operation/src/Letter.java
@@ -0,0 +1,11 @@
+
+public class Letter {
+
+	public static void main(String[] args) {
+
+		String name = "Lee";
+		System.out.println("Hello, " + name + " ... " + name + " ... Lee ... bye");
+
+	}
+
+}
---
+++ Data_and_operation/src/StringOperation.java
@@ -0,0 +1,11 @@
+
+public class StringOperation {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World".length()); // 11
+		System.out.println("Hello, Lee ... bye.".replace("Lee", "Park"));
+
+	}
+
+}
